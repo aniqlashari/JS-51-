587f6b68a229fc6eeca65f47ddd55cd8265aaa87
aniqlashari <mahaqamber3@gmail.com>
2024-07-09 19:43:23 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,8 @@
+var ques1 = prompt("where from the array you want to starting?")
+var ques2 = prompt("how many elements to delete?")
+var ques3 = ["   ANIQ LASHARi    " , "    WASAY   " , "   MUSA    " , "    FATIMA   " , "    musa    "]
+
+alert(ques3)
+
+ques3.slice(ques1,ques2)
+console.log(ques3);
